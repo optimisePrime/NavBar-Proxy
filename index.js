6fd7a73a124b91d1775ab17e17f18e37a6b56d26
ZK Liu <zkliu@ZKs-Mac.local>
2019-01-25 15:05:26 -0800

    complete proxy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// require('newrelic')
 const Express = require('express');
 const Path = require('path');
 const httpReq = require('axios');
@@ -12,6 +13,50 @@ app.get('/reviews/all/:productid', (req, res) => {
   httpReq.get(`http://reviewloadbalancer-438610705.us-west-1.elb.amazonaws.com/reviews/all/${productId}`)
   .then((reviews) => {
     res.status(200).send(reviews.data);
+  })
+  .catch((error) => {
+    res.status(501).send(error);
+  });
+})
+
+app.get('/items/:id', (req, res) => {
+  const productId = req.params.id;
+  httpReq.get(`http://ec2-3-16-24-103.us-east-2.compute.amazonaws.com/items/${productId}`)
+  .then((items) => {
+    res.status(200).send(items.data);
+  })
+  .catch((error) => {
+    res.status(501).send(error);
+  });
+})
+
+app.get('/items/:id/related', (req, res) => {
+  const productId = req.params.id;
+  httpReq.get(`http://ec2-3-16-24-103.us-east-2.compute.amazonaws.com/items/${productId}/related`)
+  .then((related) => {
+    res.status(200).send(related.data);
+  })
+  .catch((error) => {
+    res.status(501).send(error);
+  });
+})
+
+app.get('/products/:productId', function (req, res) {
+  let id = req.params.productId;
+  httpReq.get(`http://ec2-13-59-133-55.us-east-2.compute.amazonaws.com:9000/products/${id}`)
+  .then(response => {
+     res.status(200).send(response.data)
+  })
+  .catch((error) => {
+    res.status(501).send(error);
+  });
+})
+
+app.get('/photos/:productId', function (req, res) {
+  let id = req.params.productId;
+  httpReq.get(`http://ec2-13-59-133-55.us-east-2.compute.amazonaws.com:9000/photos/${id}`)
+  .then(response => {
+     res.status(200).send(response.data)
   })
   .catch((error) => {
     res.status(501).send(error);
